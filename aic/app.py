6722595d718cbf235b796fb179e40a2d75294552
Richard Plangger <rich@pasra.at>
2013-10-21 10:39:20 +0200

    implemented scraping from rss feed

--- aic/app.py
+++ aic/app.py
@@ -1,10 +1,21 @@
 
-from flask import Flask
-app = Flask(__name__)
+import application
+import threading
+
+app = application.app
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
+    print("starting background fetcher")
+    thread = threading.Thread(target=application.fetch,\
+            name="background_fetcher", args=("http://finance.yahoo.com/news/?format=rss",))
+    thread.start();
+
     app.run()
+    application.shutdown = True
+
+    print("joining background fetcher")
+    thread.join()
