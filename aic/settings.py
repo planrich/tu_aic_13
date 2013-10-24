10bfa6a07f2c790755b4d523497ab54ee0f9afa9
KonradMSteiner <konradm.steiner@gmail.com>
2013-10-24 22:52:26 +0200

    added mobile works credential to settings.py

--- aic/settings.py
+++ aic/settings.py
@@ -2,3 +2,5 @@ import os
 
 DB_URL = os.environ.get('DB_URL', 'postgresql://aic:aic@localhost/aic')
 RSS_URL = 'http://finance.yahoo.com/news/?format=rss'
+mobileWorks_Username = 'aic13lab2topic2'
+mobileWorks_Password = '1diesgp1'
