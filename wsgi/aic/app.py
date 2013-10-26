478cc7905a37fac3d84e991a80d858c6e9a4908a
Richard Plangger <planrichi@gmail.com>
2013-10-26 17:07:11 +0200

    added some logic for the front page

--- wsgi/aic/app.py
+++ wsgi/aic/app.py
@@ -1,15 +1,27 @@
 from flask import Flask
 import platform
+import db
+
 application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return 'Hello from Flask(1)'
+    sess = db.Session()
+    pubs = sess.query(db.Publication).all()
+
+    out = 'AIC 2013 crowd sourcing<br/>\n'
+
+    i = 0
+    for pub in pubs:
+        out += "scrape %d: %s,<br/>\n" % (i,str(pub.datetime))
+        i += 1
+
+    return out
 
 @application.route("/info")
 def info():
     return platform.python_version()
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    application.debug = True
+    application.run()
