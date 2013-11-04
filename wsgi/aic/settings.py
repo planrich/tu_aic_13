ea62921f15315a9e0b1b666d846640c181e768b7
KonradMSteiner <konradm.steiner@gmail.com>
2013-11-04 22:13:13 +0100

    fix error in settings

--- wsgi/aic/settings.py
+++ wsgi/aic/settings.py
@@ -6,6 +6,6 @@ RSS_URL = 'http://finance.yahoo.com/news/?format=rss'
 if os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL') is not None:
     DB_URL = os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL', 'postgresql://aic:aic@localhost/aic')
     POST_TASK_LINK = 'http://aic13lab2topic2-mobileworks.rhcloud.com/tasks'
-else
+else:
 	DB_URL = os.environ.get('DB_URL', 'postgresql://aic:aic@localhost/aic')
 	POST_TASK_LINK = 'http://127.0.0.1:5000/tasks'
