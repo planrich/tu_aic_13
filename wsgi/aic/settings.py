4be65f79d7ff8f4877f4c085617585a106a85a98
Christian Schnitzer <christian.schnitzer@rise-world.com>
2013-11-04 22:00:49 +0100

    settings extended for rest interface

--- wsgi/aic/settings.py
+++ wsgi/aic/settings.py
@@ -1,12 +1,11 @@
 import os
 
+RSS_URL = 'http://finance.yahoo.com/news/?format=rss'
 
-
-DB_URL = os.environ.get('DB_URL', 'postgresql://aic:aic@localhost/aic')
+# if deployed on openshift
 if os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL') is not None:
     DB_URL = os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL', 'postgresql://aic:aic@localhost/aic')
-
-RSS_URL = 'http://finance.yahoo.com/news/?format=rss'
-mobileWorks_Username = 'aic13lab2topic2'
-mobileWorks_Password = '1diesgp1'
-mobileWorks_Webhook = 'http://aic13lab2topic2-mobileworks.rhcloud.com/webhook'
+    CALLBACK_LINK = 'http://aic13lab2topic2-mobileworks.rhcloud.com/tasks'
+else
+	DB_URL = os.environ.get('DB_URL', 'postgresql://aic:aic@localhost/aic')
+	CALLBACK_LINK = 'http://127.0.0.1:5000/tasks'
