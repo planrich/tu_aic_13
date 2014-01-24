b54d9d7e4c15afd1c73486f68ad676d336345f71
Christian Schnitzer <christian.schnitzer@rise-world.com>
2014-01-24 19:18:44 +0100

    startScheduler now standalone

--- wsgi/aic/startScheduler.py
+++ wsgi/aic/startScheduler.py
@@ -1,4 +1,6 @@
 
+import settings
+
 # Logging
 import logging
 logger = settings.createLog("start_scheduler")
@@ -11,9 +13,14 @@ import garbage_collector
 
 if __name__ == "__main__": 
     logger.info("startScheduledJobs file")
-    sc = Scheduler()
-    sc.start()
+    sc = Scheduler(standalone=True)
     # Schedule  to be called every hour
     sc.add_interval_job(scraper.scrape, minutes=1)
     sc.add_interval_job(dynamic_pricer.dynamic_pricing, minutes=1)
     sc.add_interval_job(garbage_collector.garbage_collecting, minutes=1)
+
+    print('Press Ctrl+C to exit')
+    try:
+        sc.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
